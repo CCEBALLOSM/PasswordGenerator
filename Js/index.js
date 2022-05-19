262bc8781bce3c542ae8d7cbfa25885162dffe8f
CCEBALLOSM <105832140+CCEBALLOSM@users.noreply.github.com>
2022-05-18 21:46:13 -0500

    Add files via upload

---
+++ Js/index.js
@@ -0,0 +1,52 @@
+//Dejamos que cargue el documento para mostrar el preloader
+window.onload = function(){
+    setTimeout(function(){ 
+    $('#onload').fadeOut(); //el loader
+    $('body').removeClass('heiden');//Removenos la clase heiden de la etiqueta body
+    }, 1000);//Después de 1s ocultar el preload
+    
+  };
+
+
+  const generatePassword = (base, length) => {
+    let password = "";
+    for (let x = 0; x < length; x++) {
+        let random = Math.floor(Math.random() * base.length);
+        password += base.charAt(random);
+    }
+    return password;
+};
+
+const generate = () => {
+    let length = parseInt(inputLength.value);
+
+    let base = "abcdefghijklmnñopqrstuvwxyz";
+    const numbers = "0123456789";
+    const mayusculas = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+    const symbols = ".?+-_¡!¿*%&$/()[]{}|@><";
+
+
+    if (checkbox1.checked) base += numbers;
+
+    if (checkbox2.checked) base += symbols;
+
+    if (checkbox3.checked) base += mayusculas;
+
+    generatedPassword.innerText = generatePassword(base, length);
+};
+
+var value=$.trim($(".form-control").val());
+if(value.length>0)
+{
+    $("#user-data-next-button").attr('disabled', true);
+} else{
+    $("#user-data-next-button").attr('disabled', false);
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    btnGenerate.addEventListener("click", () => {
+        generate();
+    });
+});
+
+
